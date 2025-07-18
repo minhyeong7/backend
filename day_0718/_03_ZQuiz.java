dad651487bf96f8e7e572ad31fa28f88dda64a69
minhyeong7 <duswlsalsgud@naver.com>
2025-07-18 16:10:17 +0900

    test2

---
+++ day_0718/_03_ZQuiz.java
@@ -0,0 +1,20 @@
+package day_0718;
+
+public class _03_ZQuiz {
+    public static void main(String[] args) {
+
+        String name="김자바";
+        int age=20;
+        float height=172.5f;
+        char gender='M';
+
+        System.out.println("이름:"+name);
+        System.out.println("나이:"+age);
+        System.out.println("키:"+height+"cm");
+        System.out.println("성별:"+gender);
+
+
+
+
+    }
+}
--- day_0718/_03_variables.java
+++ day_0718/_03_variables.java
@@ -1,4 +1,33 @@
 package day_0718;
 
 public class _03_variables {
+    public static void main(String[] args) {
+        String hello = "안녕하세요";
+        int time = 10;
+
+        System.out.println(hello+" 홍길동님 "+time+"시에 만나요");
+        System.out.println(time);
+        System.out.println(hello + " 그렇게 합시다");
+
+        double height = 175.6;
+        char gender = 'M';
+        hello = "안녕";
+        System.out.println(hello+"? 키는"+height+"성별은 "+gender+"입니다");
+
+        boolean love = true;
+        System.out.println("그는 나를 좋아할까요"+love);
+
+        double d = 2.09281;
+        float e = 5.32412312f;
+        long i= 100000000000000000L;
+
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(i);
+
+
+        i=3_0000_3000L;
+        System.out.println(i);
+
+    }
 }
